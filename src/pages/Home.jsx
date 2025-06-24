e88093bacacabcaa3e8a652a9137552a0d04d111
Hubert <hubertcen93@gmail.com>
2025-06-24 14:38:14 +0700

    edit overall

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,6 @@ import { Hero } from "@/components/Hero";
 import { About } from "@/components/About";
 import { Skills } from "@/components/Skills";
 import { Projects } from "@/components/Projects";
-// import { Contact } from "@/components/Contact";
 
 export const Home = () => {
   return (
@@ -21,8 +20,6 @@ export const Home = () => {
       <About />
       <Skills />
       <Projects />
-      {/* <Contact /> */}
-      {/* Footer */}
     </div>
   );
 };
